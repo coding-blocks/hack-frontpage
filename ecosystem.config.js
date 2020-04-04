d694009141625e715fc368b9d3fde2b728b81eb3
Jatin Katyal <jatin.katyal13@gmail.com>
2020-04-04 20:49:03 +0530

    fix pm2 deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
     script: 'yarn start',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    instances: 4,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',

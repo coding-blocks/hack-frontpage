d0c0fd6dc62900277424725758f39efa469125ab
Jatin Katyal <jatin.katyal13@gmail.com>
2020-04-04 19:34:06 +0530

    setup deploy pipeline

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps : [{
+    name: 'hack-frontpage',
+    append_env_to_name: true,
+    script: './.nuxt/app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 4,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env_production: {
+      NODE_ENV: 'production',
+      ...require('./secrets/production.json')
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};

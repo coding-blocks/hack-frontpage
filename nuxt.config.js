9bd386f6f8dcf0a00cd05a4ad1694ebcff60b7b5
Vibhu Dujari <vdvibhu20@gmail.com>
2022-10-02 21:01:03 +0530

    web-components@0.2.3

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
-      { src: 'https://unpkg.com/@coding-blocks/web-components@0.2.2/dist/index.js' },
+      { src: 'https://unpkg.com/@coding-blocks/web-components@0.2.3/dist/index.js' },
       { src: 'https://code.jivosite.com/widget.js', 'data-jv-id': 'ASYGLZQ6UC' }
     ]
   },

f2d60d5a66a5e380d5a028655379cb73c18556ec
Vibhu Dujari <vdvibhu20@gmail.com>
2022-12-24 04:23:01 +0530

    redirect to https

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div id="g_id_onload"
        data-client_id="17928091484-k5aqk9hr1orcoqj0373nqdfft91ncfsf.apps.googleusercontent.com"
        data-auto_prompt="true"
-       data-redirect_to="http://hack.codingblocks.com"
+       data-redirect_to="https://hack.codingblocks.com"
        data-client_name="hackerblocks-codingblocks"
        data-oneauth_client_id="2146237097"
        data-login_uri="https://account.codingblocks.com/login/google/onetap-signin-callback">

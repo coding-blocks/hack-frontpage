256bf2aff98ed5ace15dd71a0489bde97ce62da9
Vibhu Dujari <vdvibhu20@gmail.com>
2022-12-24 02:37:07 +0530

    add data-auto_prompt

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <div id="g_id_onload"
        data-client_id="17928091484-k5aqk9hr1orcoqj0373nqdfft91ncfsf.apps.googleusercontent.com"
        data-auto_select="true"
+       data-auto_prompt="true"
        data-login_uri="https://account.codingblocks.com/login/google/callback-v2">
     </div>
     <LandingPage />

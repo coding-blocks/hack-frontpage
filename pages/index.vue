b8ae373c0f8ce7e9c234c1dc47576540178f5735
Vibhu Dujari <vdvibhu20@gmail.com>
2022-12-24 02:46:48 +0530

    data-auto_select

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <div>
     <div id="g_id_onload"
        data-client_id="17928091484-k5aqk9hr1orcoqj0373nqdfft91ncfsf.apps.googleusercontent.com"
-       data-auto_select="true"
        data-auto_prompt="true"
        data-login_uri="https://account.codingblocks.com/login/google/callback-v2">
     </div>

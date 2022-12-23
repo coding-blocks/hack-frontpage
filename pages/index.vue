b3f60ae6a640bdaa3d476ca842946cb6ffae3af9
Vibhu Dujari <vdvibhu20@gmail.com>
2022-12-24 04:14:20 +0530

    add valid data attributes to onetap signin

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
     <div id="g_id_onload"
        data-client_id="17928091484-k5aqk9hr1orcoqj0373nqdfft91ncfsf.apps.googleusercontent.com"
        data-auto_prompt="true"
-       data-login_uri="https://account.codingblocks.com/login/google/callback-v2">
+       data-redirect_to="http://hack.codingblocks.com"
+       data-client_name="hackerblocks-codingblocks"
+       data-oneauth_client_id="2146237097"
+       data-login_uri="https://account.codingblocks.com/login/google/onetap-signin-callback">
     </div>
     <LandingPage />
   </div>
